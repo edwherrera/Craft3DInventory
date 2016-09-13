8c0d6499bed8110534639c1af980110960a9ea86
Edwin Herrera <edwherrera.175@gmail.com>
2016-09-13 17:50:40 -0600

    cleaned main module

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
 from service import service
-from exception import exception
-
-
-def main():
-    printer_service = service.Printer()
-
-    try:
-        print(printer_service.get(model='Printe'))
-    except exception.ObjectNotFoundException:
-        print('The object already exists')
-
-
-if __name__ == '__main__':
-    main()
